5fb52e062fbede3c258751b2a464472d4a6fbb46
Kimiko <kimiko@terraphim.ai>
2026-02-23 10:13:25 +0800

    fix: Get repo from query params instead of context
    Refs #6

--- routers/api/v1/robot/robot.go
+++ routers/api/v1/robot/robot.go
@@ -6,22 +6,41 @@ package robot
 import (
 	"net/http"
 
-	"code.gitea.io/gitea/services/context"
+	repo_model "code.gitea.io/gitea/models/repo"
 	"code.gitea.io/gitea/modules/setting"
+	"code.gitea.io/gitea/services/context"
 	"code.gitea.io/gitea/services/robot"
 )
 
 // Triage returns prioritized list of issues for agents
 func Triage(ctx *context.APIContext) {
 	if !setting.IssueGraph.Enabled {
-		ctx.APIError(http.StatusNotFound, "Issue graph features are disabled")
+		ctx.APIErrorNotFound("Issue graph features are disabled")
+		return
+	}
+
+	owner := ctx.FormString("owner")
+	repoName := ctx.FormString("repo")
+
+	if owner == "" || repoName == "" {
+		ctx.APIError(http.StatusBadRequest, "owner and repo query parameters are required")
+		return
+	}
+
+	repo, err := repo_model.GetRepositoryByOwnerAndName(ctx, owner, repoName)
+	if err != nil {
+		ctx.APIErrorInternal(err)
+		return
+	}
+	if repo == nil {
+		ctx.APIErrorNotFound("Repository not found")
 		return
 	}
 
 	svc := robot.NewService()
-	response, err := svc.Triage(ctx, ctx.Repo.Repository.ID)
+	response, err := svc.Triage(ctx, repo.ID)
 	if err != nil {
-		ctx.APIError(http.StatusInternalServerError, err)
+		ctx.APIErrorInternal(err)
 		return
 	}
 
